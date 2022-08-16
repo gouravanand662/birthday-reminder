37f63ba691cd84a17bbbf29b766aca836cfb8b1d
Gourav Anand <gouravanand662@gmail.comgit config --global user.email gouravanand662@gmail.comgit config --global user.email gouravanand662@gmail.com>
2022-08-17 05:07:50 +0530

    Updates

---
+++ precache-manifest.625cb06965f0c73f046d396b5ba47531.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3153faf29bd36fb444afaec6eaba396",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "d298c3dca1a710930c9c",
+    "url": "/birthday-reminder/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "a9126082705d5b5d431b",
+    "url": "/birthday-reminder/static/js/2.daefb4dd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/birthday-reminder/static/js/2.daefb4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d298c3dca1a710930c9c",
+    "url": "/birthday-reminder/static/js/main.387ca889.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
